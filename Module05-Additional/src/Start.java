efad1e3a6049d13d2da28e475581a396ec1f3caa
sirxeed <sir.xeed@gmail.com>
2016-01-06 00:53:03 +0200

    some bugs fixed

--- Module05-Additional/src/Start.java
+++ Module05-Additional/src/Start.java
@@ -30,15 +30,37 @@ public class Start {
                 counter++;
             }
         }
+        matrix[2][0] += 1;
 
+        //Для дебагінга №2
+        matrix[0][0] = -1;
+        matrix[0][1] = -8;
+        matrix[0][2] = 4;
+        matrix[0][3] = -9;
+
+        matrix[1][0] = -4;
+        matrix[1][1] = 5;
+        matrix[1][2] = -7;
+        matrix[1][3] = -3;
+
+        matrix[2][0] = -6;
+        matrix[2][1] = 2;
+        matrix[2][2] = 9;
+        matrix[2][3] = 2;
+
+        matrix[3][0] = -4;
+        matrix[3][1] = -9;
+        matrix[3][2] = 6;
+        matrix[3][3] = -5;
         */
+
+        //Друкуємо вихідну матрицю
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
-                System.out.print(matrix[j][i] + "   ");
+                System.out.print(matrix[j][i] + " \t\t\t");
             }
             System.out.println();
         }
-        matrix[2][0] += 1;
 
         System.out.print("Matrix determinant is: " + MatrixCalculator.determinant(matrix));
     }
