c363e5ea0455c9dd873cac7464dee13939c2f659
Andrii Stasiuk <sir.xeed@gmail.com>
2016-01-21 20:58:43 +0200

    improved method overloading

--- Module09/src/EncryptUtils.java
+++ Module09/src/EncryptUtils.java
@@ -2,37 +2,21 @@ public class EncryptUtils {      //used this: http://rosettacode.org/wiki/Caesar
     private static final int DEFAULT_CAESAR_SHIFT = 7;  //Shift key
     private static final int DEFAULT_ALPHABET_SIZE = 26;  //Alphabet
 
-    public static String caesarEncryptionMethod(String string, boolean isEncrypted) {
-        char[] chars = string.toCharArray();
-        StringBuilder crypted = new StringBuilder();
-
-        for (char i : chars) {
-            if (Character.isLetter(i)) {
-                if (Character.isUpperCase(i)) {
-                    crypted.append((char) ('A' + (i - 'A' + DEFAULT_CAESAR_SHIFT * encrypted(isEncrypted)) % DEFAULT_ALPHABET_SIZE));
-                    System.out.println(encrypted(isEncrypted));
-                } else {
-                    crypted.append((char) ('a' + (i - 'a' + DEFAULT_CAESAR_SHIFT * encrypted(isEncrypted)) % DEFAULT_ALPHABET_SIZE));
-                }
-            } else {
-                crypted.append(i);
-            }
-        }
-
-        return crypted.toString();
+    public static String caesarEncryptionMethod(String string, boolean encrypt) {
+        return caesarEncryptionMethod(string, encrypt, DEFAULT_CAESAR_SHIFT);
     }
 
-    public static String caesarEncryptionMethod(String string, boolean isEncrypted, int shift) {
+    public static String caesarEncryptionMethod(String string, boolean encrypt, int shift) {
         char[] chars = string.toCharArray();
         StringBuilder crypted = new StringBuilder();
 
         for (char i : chars) {
             if (Character.isLetter(i)) {
                 if (Character.isUpperCase(i)) {
-                    crypted.append((char) ('A' + (i - 'A' + shift * encrypted(isEncrypted)) % DEFAULT_ALPHABET_SIZE));
-                    System.out.println(encrypted(isEncrypted));
+                    crypted.append((char) ('A' + (i - 'A' + shift * encrypted(encrypt)) % DEFAULT_ALPHABET_SIZE));
+                    System.out.println(encrypted(encrypt));
                 } else {
-                    crypted.append((char) ('a' + (i - 'a' + shift * encrypted(isEncrypted)) % DEFAULT_ALPHABET_SIZE));
+                    crypted.append((char) ('a' + (i - 'a' + shift * encrypted(encrypt)) % DEFAULT_ALPHABET_SIZE));
                 }
             } else {
                 crypted.append(i);
