44c27c36b2202806ebef7069c13005ba6c52fa71
Andrii Stasiuk <sir.xeed@gmail.com>
2016-01-23 22:48:12 +0200

    created 'textScanner' = new Scanner(System.in) object in editing() because of strange behavior of Scanner.nextLine();

--- Module10/src/Start.java
+++ Module10/src/Start.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class Start {
     public static void main(String[] args) throws IOException {
